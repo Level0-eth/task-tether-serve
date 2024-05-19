058d554b7661ae82f7bf113a5e2250cb841b7106
shubrautt <shubham162001@gmail.com>
2024-05-20 03:42:18 +0530

    refector:rename router to routes

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 import express from "express";
-import userRouter from "./routers/user";
+import userRoute from "./routes/user";
 import bodyParser from "body-parser";
 
 import { dbconnection } from "./config/db";
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 dbconnection();
 
-app.use("/v1/user", userRouter);
+app.use("/v1/user", userRoute);
 
 app.listen(process.env.PORT, () => {
   console.log("server is runnig ", process.env.PORT);
