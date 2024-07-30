6c79fe54ac145a856785e62289d691fbf9aa1603
shubrautt <shubham162001@gmail.com>
2024-07-30 23:43:16 +0530

    feat:cors

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,21 @@ require("dotenv").config();
 
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 import userRoute from "./routes/userRoute";
 import { dbconnection } from "./config/db";
 
 const app = express();
+
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
