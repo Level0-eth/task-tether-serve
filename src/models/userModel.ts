67cbe43d0d933845552f41fa7f2a5d47d7cdf313
shubrautt <shubham162001@gmail.com>
2024-08-18 16:30:35 +0530

    feat: default removed from all fields for user schema

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -12,28 +12,22 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
-        default: "N/A",
     },
     lastName: {
         type: String,
         required: true,
-        default: "N/A",
     },
     photoUrl: {
         type: String,
         required: true,
-        unique: true,
-        default: "N/A",
     },
     authDate: {
         type: String,
         required: true,
-        default: "N/A",
     },
     chatID: {
         type: String,
         required: true,
-        default: "N/A",
     }
 });
 
