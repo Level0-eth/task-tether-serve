4cb9212f3cde215f1aeec8bdca7b0c022540d931
shubrautt <shubham162001@gmail.com>
2024-09-03 00:09:56 +0530

    feat: get list route

--- src/routes/listRoute.ts
+++ src/routes/listRoute.ts
@@ -1,10 +1,11 @@
 import { Request, Response, Router } from "express";
 
 import validateUser from "../middlewares/validateUser";
-import { createList } from "../controllers/listController";
+import { createList, getLists } from "../controllers/listController";
 
 const router = Router();
 
 router.post("/createList", validateUser, createList);
+router.get("/getLists", validateUser, getLists);
 
 export default router;
