845dcf4e398f0c5bece0d30146075ae7bac9fa43
shubrautt <shubham162001@gmail.com>
2024-09-07 16:04:31 +0530

    feat: delete task

--- src/routes/taskRoute.ts
+++ src/routes/taskRoute.ts
@@ -58,4 +58,53 @@ router.post(
   }
 );
 
+router.post(
+  "/deleteTask/:id/:taskId",
+  validateUser,
+  async (req: Request, res: Response) => {
+    const { user } = req.body;
+    const listId = req.params.id;
+    const taskId = req.params.taskId;
+
+    let session;
+
+    try {
+      session = await mongoose.startSession();
+      session.startTransaction();
+
+      const task = await TaskModel.findByIdAndDelete(taskId, { session });
+
+      if (task == null) {
+        throw new Error("Task not found");
+      }
+
+      await UserModel.findOneAndUpdate(
+        {
+          _id: user.id,
+          "lists._id": listId,
+        },
+        { $pull: { "lists.$.tasks": taskId } },
+        { new: true, session }
+      );
+
+      await session.commitTransaction();
+      await session.endSession();
+
+      res.status(200).json({
+        message: "Task is Deleted",
+      });
+    } catch (err) {
+      if (session) {
+        await session.abortTransaction();
+        await session.endSession();
+      }
+
+      res.status(400).json({
+        message: "Task deletion is failed",
+        data: null,
+      });
+    }
+  }
+);
+
 export default router;
